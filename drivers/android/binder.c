602edad91ae5bc2a5018122a8639c5055acc8534
Kaushal Kumar <kaushalk@codeaurora.org>
2015-08-07 20:09:41 +0530

    Revert "android: binder: Change binder mutex to rtmutex."
    This reverts commit 7e157bd483e7 ("android: binder: Change
    binder mutex to rtmutex."). The original patch that originated
    from AOSP kernel is leading to some performance degradation.
    The patch is already reverted in Google kernel/msm tree. The
    reason could be the extra overhead involved with RT mutex
    causing performance issues.
    
    Change-Id: I524dff640425bfa7d4ee5533b9eb778717805230
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -26,7 +26,6 @@
 #include <linux/miscdevice.h>
 #include <linux/mm.h>
 #include <linux/module.h>
-#include <linux/rtmutex.h>
 #include <linux/mutex.h>
 #include <linux/nsproxy.h>
 #include <linux/poll.h>
@@ -47,7 +46,7 @@
 #include <uapi/linux/android/binder.h>
 #include "binder_trace.h"
 
-static DEFINE_RT_MUTEX(binder_main_lock);
+static DEFINE_MUTEX(binder_main_lock);
 static DEFINE_MUTEX(binder_deferred_lock);
 static DEFINE_MUTEX(binder_mmap_lock);
 
@@ -430,14 +429,14 @@ static long task_close_fd(struct binder_proc *proc, unsigned int fd)
 static inline void binder_lock(const char *tag)
 {
 	trace_binder_lock(tag);
-	rt_mutex_lock(&binder_main_lock);
+	mutex_lock(&binder_main_lock);
 	trace_binder_locked(tag);
 }
 
 static inline void binder_unlock(const char *tag)
 {
 	trace_binder_unlock(tag);
-	rt_mutex_unlock(&binder_main_lock);
+	mutex_unlock(&binder_main_lock);
 }
 
 static inline long copy_from_user_preempt_disabled(void *to, const void __user *from, long n)
