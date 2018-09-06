ShieldKteam/android_kernel_motorola_msm8916

enable dt2w halfscreen

drivers/input/touchscreen/doubletap2wake.c
@@ -67,7 +67,7 @@ static struct work_struct dt2w_input_work;
 static int __init read_dt2w_cmdline(char *dt2w)
 {
 	if (strcmp(dt2w, "1") == 0) {
-		pr_info("[cmdline_dt2w]: DoubleTap2Wake enabled. | dt2w='%s'\n", dt2w);
+		pr_info("[cmdline_dt2w]: DoubleTap2Wake halscrren enabled. | dt2w='%s'\n", dt2w);
 		dt2w_switch = 1;
 	} else if (strcmp(dt2w, "2") == 0) {
 		pr_info("[cmdline_dt2w]: DoubleTap2Wake fullscreen enabled. | dt2w='%s'\n", dt2w);
@@ -137,10 +137,8 @@ static void detect_doubletap2wake(int x, int y, bool st)
         pr_info(LOGTAG"x,y(%4d,%4d) single:%s\n",
                 x, y, (single_touch) ? "true" : "false");
 #endif
-	if (x < 100 || x > 980)
-        	return;
 
-	if (dt2w_switch < 2 && y < 1000)
+	if ((dt2w_switch == 0) || ((dt2w_switch == 1) && (y < 640)))
         	return;
 
 	if ((single_touch) && (dt2w_switch > 0) && (exec_count) && (touch_cnt)) {

FacuM/android_kernel_motorola_msm8916-2

drivers/input/touchscreen/doubletap2wake.c
@@ -68,7 +68,7 @@ static struct work_struct dt2w_input_work;
 static int __init read_dt2w_cmdline(char *dt2w)
 {
 	if (strcmp(dt2w, "1") == 0) {
-		pr_info("[cmdline_dt2w]: DoubleTap2Wake enabled. | dt2w='%s'\n", dt2w);
+		pr_info("[cmdline_dt2w]: DoubleTap2Wake halfscreen enabled. | dt2w='%s'\n", dt2w);
 		dt2w_switch = 1;
 	} else if (strcmp(dt2w, "2") == 0) {
 		pr_info("[cmdline_dt2w]: DoubleTap2Wake fullscreen enabled. | dt2w='%s'\n", dt2w);
@@ -138,10 +138,8 @@ static void detect_doubletap2wake(int x, int y, bool st)
         pr_info(LOGTAG"x,y(%4d,%4d) single:%s\n",
                 x, y, (single_touch) ? "true" : "false");
 #endif
-	if (x < 100 || x > 980)
-        	return;
 
-	if (dt2w_switch < 2 && y < 1000)
+	if ((dt2w_switch == 0) || ((dt2w_switch == 1) && (y < 640)))
         	return;
 
 	if ((single_touch) && (dt2w_switch > 0) && (exec_count) && (touch_cnt)) {